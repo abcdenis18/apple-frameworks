ed0c821a007ee63fa7505a117b2479a348ef271d
abcdenis18 <denisfajar24@gmail.com>
2023-11-05 16:05:07 +0700

    Initial Commit

---
+++ Apple Framework/Apple_FrameworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FrameworkApp.swift
+//  Apple Framework
+//
+//  Created by Denis Fajar Sidik on 05/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Apple_FrameworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
